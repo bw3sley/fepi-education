62e8cff91799ae86e642ae4e2cb2167ab6aa402f
bw3sley <bw3sley@gmail.com>
2026-05-13 12:58:57 -0300

    📁  chore: organize showcases folders

---
+++ general/showcases/2023/games/3d-modeling/main.js
@@ -0,0 +1,104 @@
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input[type='radio']");
+const button  = document.querySelector("button[type='submit']");
+
+for (const input of inputs) {
+    input.addEventListener("click", () => {
+        const checkedInputs = document.querySelectorAll("input[type='radio']:checked");
+        
+        if (checkedInputs.length === 5) {
+            button.removeAttribute("disabled");
+        } 
+        
+        else {
+            button.setAttribute("disabled", "disabled");
+        }
+    });
+}
+
+const rightAnswers = [
+    {
+        id: "question-1",
+        answer: "entry"
+    },
+    {
+        id: "question-2",
+        answer: "entry"
+    },
+    {
+        id: "question-3",
+        answer: "entry"
+    },
+    {
+        id: "question-4",
+        answer: "4"
+    },
+    {
+        id: "question-5",
+        answer: "2"
+    }
+];
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const userAnswers = [];
+
+    const answers = document.querySelectorAll("input[type='radio']:checked");
+
+    for (const answer of answers) {
+        const selectedQuestion =  answer.name;
+        const selectedAnswer = answer.value;
+
+        const rightQuestion = rightAnswers.find(question => question.id === selectedQuestion);
+
+        if (rightQuestion.answer === selectedAnswer) {
+            userAnswers.push(selectedAnswer);
+        }
+
+        else {
+            answer.parentElement.classList.add("wrong-answer");
+        }
+    }
+
+    document.querySelector("#final-answer").classList.remove("hidden");
+    document.querySelector("#final-question").classList.remove("hidden");
+
+    const wrapper = document.querySelector("#final-message");
+
+    const strong = document.createElement("strong");
+    const p = document.createElement("p");
+    const reloadButton = document.createElement("button");
+    
+    strong.classList.add("text-2xl");
+
+    reloadButton.type = "button";
+    reloadButton.classList.add("reload-button");
+    reloadButton.innerHTML = `<i class="ph-fill ph-arrow-clockwise"></i> Tentar novamente`;
+
+    if (userAnswers.length === 5) {
+        strong.textContent = "Parabéns! Você acertou todas as questões! 🎉👏";
+
+        p.textContent = "Você é realmente incrível e demonstrou um conhecimento impressionante. Continue assim e continue se desafiando. Continue com esse espírito vencedor! Para retirar seu brinde, tire uma foto do placar, poste em seu Instagram marcando @fepi.siads e mostre para algum aluno.";
+    }
+
+    else if (userAnswers.length >= 1 && userAnswers.length <= 4) {
+        strong.textContent = `Parabéns! Você acertou entre ${userAnswers.length} e 5 questões! 🎉`;
+
+        p.textContent = "Isso mostra que você tem um bom conhecimento e habilidades. Continue praticando e estudando para melhorar ainda mais. Cada acerto é um passo na direção certa, e você está no caminho certo para o sucesso. Continue assim! Para retirar seu brinde, tire uma foto do placar, poste em seu Instagram marcando @fepi.siads e mostre para algum aluno.";
+    }
+
+    else {
+        strong.textContent = "Você não acertou nenhuma questão, porém não desanime! Às vezes, errar é apenas um passo em direção ao sucesso";
+
+        p.textContent = "Mesmo que você não tenha acertado nenhuma questão neste quiz, lembre-se de que é uma oportunidade de aprender e melhorar. Use isso como motivação para estudar mais e se preparar melhor da próxima vez. O importante é continuar tentando e nunca desistir. Seu esforço será recompensado, e você certamente vai progredir. Você pode refazer o quiz para conseguir seu brinde.";
+    }
+
+    wrapper.append(strong, p, reloadButton);
+
+    button.setAttribute("disabled", true);
+
+    reloadButton.addEventListener("click", (event) => location.reload())
+
+    wrapper.scrollIntoView();
+};
---
+++ general/showcases/2024/src/data.mjs
@@ -0,0 +1,111 @@
+export const projects = [
+    {
+        id: "flappy-bird",
+        name: "Flappy Bird",
+        description: "Versão web do clássico jogo Flappy Bird, onde o jogador precisa desviar de obstáculos enquanto mantém o pássaro voando. Uma implementação fiel que oferece desafio e diversão.",
+        type: "Jogo",
+        position: 1,
+        coverUrl: "/assets/flappy-bird/cover.png",
+        url: "../projects/flappy-bird/index.html"
+    },
+
+    {
+        id: "snake-game",
+        name: "Snake Game",
+        description: "Implementação do jogo da cobrinha, em que o jogador controla a cobra que cresce ao comer os alimentos. Um clássico que testa a habilidade de reação e planejamento.",
+        type: "Jogo",
+        position: 2,
+        coverUrl: "/assets/snake/cover.png",
+        url: "../projects/snake/index.html"
+    },
+
+    {
+        id: "memory-game",
+        name: "Jogo da Memória",
+        description: "Desafie sua memória com este jogo interativo, onde você deve combinar pares de cartas viradas. Cada partida oferece um novo desafio e uma forma divertida de testar sua capacidade de memorização.",
+        type: "Jogo",
+        position: 3,
+        coverUrl: "/assets/memory-game/cover.png",
+        url: "https://interacty.me/projects/701373f0c3f18cc0"
+    },
+
+    {
+        id: "jogo-da-velha",
+        name: "Jogo da Velha",
+        description: "Versão digital do famoso jogo da velha, onde dois jogadores se enfrentam para completar uma linha, coluna ou diagonal com seus símbolos (X ou O).",
+        type: "Jogo",
+        position: 4,
+        coverUrl: "/assets/tic-tac-toe/cover.png",
+        url: "../projects/tic-tac-toe/index.html"
+    },
+
+    {
+        id: "pokedex",
+        name: "Pokedex",
+        description: "Aplicação web que consome dados de uma API de Pokémons e renderiza as informações de forma amigável e visualmente organizada, permitindo a navegação por diferentes Pokémons.",
+        type: "Projeto",
+        position: 5,
+        coverUrl: "/assets/pokedex/cover.png",
+        url: "../projects/pokedex/index.html"
+    },
+
+    {
+        id: "movie-ai",
+        name: "Movie-AI",
+        description: "Aplicação que consome uma API de filmes, exibindo de maneira dinâmica os filmes que estão em alta no momento. Ideal para quem quer se atualizar sobre os lançamentos mais populares.",
+        type: "Projeto",
+        position: 6,
+        coverUrl: "/assets/movie-ai/cover.png",
+        url: "../projects/movie-ai/index.html"
+    },
+
+    {
+        id: "piano-web",
+        name: "Piano",
+        description: "Aplicação interativa que permite tocar um piano virtual diretamente do navegador, simulando as notas de um piano de verdade de forma responsiva.",
+        type: "Projeto",
+        position: 7,
+        coverUrl: "/assets/piano/cover.png",
+        url: "../projects/piano/index.html"
+    },
+
+    {
+        id: "nubank-clone",
+        name: "Clone do Nubank",
+        description: "Aplicação mobile que replica a interface do aplicativo do Nubank, utilizando React Native para proporcionar uma experiência similar ao app original, com animações e navegação fluida.",
+        type: "Projeto",
+        position: 8,
+        coverUrl: "/assets/nubank-clone/cover.png",
+        url: "https://www.youtube.com/embed/cceWrZlwYNc?si=hmB8CYA0mvHP-oC4"
+    },
+
+    {
+        id: "spotify-clone",
+        name: "Clone do Spotify",
+        description: "Aplicação mobile que replica a interface do Spotify, desenvolvida com React Native, oferecendo uma experiência visual e interativa parecida com o aplicativo de streaming de música.",
+        type: "Projeto",
+        position: 9,
+        coverUrl: "/assets/spotify-clone/cover.png",
+        url: "https://www.youtube.com/embed/B3I8nTg49t0?si=RnwB9cW3FjvfmUz-"
+    },
+
+    {
+        id: "be-the-hero",
+        name: "Calculadora de Notas",
+        description: "Aplicativo para calcular notas dos alunos da faculdade, inicialmente desenvolvido com App Inventor e posteriormente refeito em React Native. A documentação do projeto, criada com MkDocs, está disponível para visualização.",
+        type: "Projeto",
+        position: 10,
+        coverUrl: "/assets/be-the-hero/cover.png",
+        url: "https://bw3sley-dev.github.io/not-the-hero/"
+    },
+
+    {
+        id: "finance-management",
+        name: "Gestão Financeira",
+        description: "Software de gestão financeira onde o usuário pode salvar suas notas fiscais e visualizar um balanço das suas entradas e saídas, facilitando o controle financeiro de maneira prática e organizada.",
+        type: "Projeto",
+        position: 11,
+        coverUrl: "/assets/finance-management/cover.png",
+        url: "/assets/finance-management/video.webm"
+    }
+];
